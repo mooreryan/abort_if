f3c23e43ffe9269d29235d45ab1c57e6c6588d3d
Ryan Moore <moorer@udel.edu>
2016-03-01 13:02:33 -0500

    Add some methods

--- lib/abort_if.rb
+++ lib/abort_if.rb
@@ -2,14 +2,33 @@ require "abort_if/version"
 require "logger"
 
 module AbortIf
-  def self.abort_if test, msg="Fatal error"
-    unless test
-      self.logger.fatal msg
+  def abort_if test, msg="Fatal error"
+    if test
+      logger.fatal msg
       abort
     end
   end
 
-  def self.logger
+  def abort_unless test, msg="Fatal error"
+    abort_if !test, msg
+  end
+
+  def logger
     @@logger ||= Logger.new STDERR
+  end
+
+  module Assert
+    class AssertionFailureError < Exception
+    end
+
+    def assert test, msg="Assertion failed", *args
+      unless test
+        raise AssertionFailureError, msg % args
+      end
+    end
+
+    def refute test, msg="Assertion failed", *args
+      assert !test, msg, *args
+    end
   end
 end
--- spec/abort_if_spec.rb
+++ spec/abort_if_spec.rb
@@ -1,36 +1,101 @@
 require 'spec_helper'
 
 describe AbortIf do
-
+  let(:klass) { Class.new { extend AbortIf } }
   let(:default_msg) { "Fatal error" }
+  let(:hash) { { a: 1, b: 2 } }
+  let(:true_test) { hash.has_key? :a }
+  let(:false_test) { hash.has_key? :c }
 
   it 'has a version number' do
     expect(AbortIf::VERSION).not_to be nil
   end
 
-  describe "::abort_if" do
-    it "returns nil if test is truthy" do
-      test = :apple
-      expect(AbortIf.abort_if test).to be nil
+  describe "#abort_if" do
+    it "returns nil if test is false" do
+      expect(klass.abort_if false_test).to be nil
     end
 
-    it "raises SystemExit if false or nil" do
-      test = false
-      expect { AbortIf.abort_if test }.to raise_error SystemExit
+    it "raises SystemExit if truthy" do
+      test = hash.has_key? :a
+      expect { klass.abort_if true_test }.to raise_error SystemExit
     end
 
     it "logs a fatal error" do
-      expect(AbortIf.logger).to receive(:fatal).with default_msg
+      expect(klass.logger).to receive(:fatal).with default_msg
 
       begin
-        AbortIf.abort_if false
+        klass.abort_if true_test
       rescue SystemExit; end
     end
   end
 
-  describe "::logger" do
+  describe "#abort_unless" do
+    it "doesn't raise SystemExit if test is true" do
+      expect(klass.abort_unless true_test).to be nil
+    end
+
+    it "raises SystemExit if test is false" do
+      expect { klass.abort_unless false_test }.
+        to raise_error SystemExit
+    end
+  end
+
+  describe "#logger" do
     it "returns the module's Logger" do
-      expect(AbortIf.logger).to be_an_instance_of Object::Logger
+      expect(klass.logger).to be_an_instance_of Object::Logger
+    end
+  end
+end
+
+describe AbortIf::Assert::AssertionFailureError do
+  it "is a kind of Exception" do
+    err = AbortIf::Assert::AssertionFailureError.new
+    expect(err).to be_an Exception
+  end
+end
+
+describe AbortIf::Assert do
+  let(:klass) { Class.new { extend AbortIf::Assert } }
+  let(:passing_test) { 1 == 1 }
+  let(:failing_test) { 1 == 2 }
+  let(:default_msg) { "Assertion failed" }
+  let(:msg) { "%d doesn't equal %d" }
+  let(:msg_output) { "1 doesn't equal 2" }
+
+  describe "#assert" do
+    it "returns nil if test is truthy" do
+      expect(klass.assert passing_test).to be nil
+    end
+
+    context "with no message specified" do
+      it "raises AbortIf::AssertionFailureError if test is false" do
+        expect { klass.assert failing_test } .
+          to raise_error(AbortIf::Assert::AssertionFailureError,
+                         default_msg)
+      end
+    end
+
+    context "with a message" do
+      it "raises and interpolates message arguments" do
+        msg = "%d doesn't equal %d"
+
+        expect { klass.assert failing_test, msg, 1, 2 } .
+          to raise_error(AbortIf::Assert::AssertionFailureError,
+                         "1 doesn't equal 2")
+      end
+    end
+  end
+
+  describe "#refute" do
+    it "returns nil if test is false" do
+      expect(klass.refute failing_test).to be nil
+    end
+
+    it "raises AssertionFailureError when test is truthy" do
+      expect { klass.refute passing_test, msg, 1, 2 } .
+        to raise_error(AbortIf::Assert::AssertionFailureError,
+                       msg_output)
     end
   end
 end
