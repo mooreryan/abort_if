5da7a74a5bc6b79e9a06eabcd554ca997b4a601a
Ryan Moore <moorer@udel.edu>
2016-03-06 19:25:08 -0500

    Add coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,9 @@
 # You should have received a copy of the GNU General Public License
 # along with AbortIf.  If not, see <http://www.gnu.org/licenses/>.
 
+require "coveralls"
+Coveralls.wear!
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'abort_if'
 
